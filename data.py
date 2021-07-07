4c65699fc8e462279779496a7d2be6115ab5e92b
abhimanyu1998 <info4abhimanyu@gmail.com>
2021-07-07 11:13:53 +0530

    first-commit

---
+++ data.py
@@ -0,0 +1,25 @@
+def Articles():
+    articles = [
+        {
+            'id':1,
+            'title':'Pulvama',
+            'body':'Pulvama attacl',
+            'authour':'jaish-e-mohhammad',
+            'create-date':'15-05-2001'
+        },
+        {
+            'id':2,
+            'title':'Balakot Air Strike',
+            'body':'Indian Air force jai hind',
+            'authour':'India n Air Force',
+            'create-date':'26-08-2019'
+        },
+        {
+            'id':3,
+            'title':'Dogfight',
+            'body':'pakistan tried to take revenge',
+            'authour':'pakistan iaf',
+            'create-date':'29-08-2019'
+        }
+    ]
+    return articles
